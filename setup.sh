6a40d5401b5f5043ba9bf36d8105cbf09c1eca53
skawashi <skawashi.engineer@gmail.com>
2022-08-31 17:01:18 +0900

    init.vimのリンクを貼るように設定を追加

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
  #!/bin/bash
 
-  DOT_FILES=(.p10k.zsh .tmux.conf .vimrc .zshrc)
+DOT_FILES=(.p10k.zsh .tmux.conf .vimrc .zshrc init.vim)
 
- for file in ${DOT_FILES[@]}
- do
-     ln -s $HOME/dotfiles/$file $HOME/$file
- done
+for file in ${DOT_FILES[@]}
+do
+  if [ $file = init.vim ]; then
+    ln -s $HOME/dotfiles/$file $HOME/.config/nvim/$file
+  else
+    ln -s $HOME/dotfiles/$file $HOME/$file
+  fi
+done
